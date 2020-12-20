1c897cfdb72bd5d7b4339ff152b8cddb043ae10b
Aymane Hrouch <jo.aymane@gmail.com>
2020-12-20 03:51:21 +0100

    fixed port

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 var cors = require("cors");
-app.use(cors())
+app.use(cors());
 
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/logging")();
 require("./startup/config")();
-require("./startup/prod")(app)
+require("./startup/prod")(app);
 require("./middleware/ok")(app);
 
-
-app.listen(3900);
+const port = process.env.PORT || 3000;
+app.listen(port);
