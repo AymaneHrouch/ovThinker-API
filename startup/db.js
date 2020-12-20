a020aadd1786b1dc77ac0573242d2ebd91612c49
Aymane Hrouch <jo.aymane@gmail.com>
2020-12-20 03:13:53 +0100

    add supp for conn via an env variable

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,10 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require("config")
 
 module.exports = function () {
   mongoose
-    .connect("mongodb://localhost/mini", {
+    .connect(config.get("db"), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
