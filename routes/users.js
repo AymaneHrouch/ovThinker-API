10451bef016e3cd1ce5cac9c574681bdb74280b9
Aymane Hrouch <jo.aymane@gmail.com>
2021-01-22 11:19:09 +0000

    add PUT method

--- routes/users.js
+++ routes/users.js
@@ -5,30 +5,52 @@ const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
+const Joi = require("joi");
 
-router.get('/', [auth, admin], async (req, res) => {
-    let users = await User.find();
-    res.send(users);
-})
+router.get("/", [auth, admin], async (req, res) => {
+  let users = await User.find();
+  res.send(users);
+});
 
-router.get('/:id', auth, async (req, res) => {
-    let user = await User.findById(req.params.id).select('-password -isAdmin')
-    res.send(user)
-})
+router.get("/:id", auth, async (req, res) => {
+  let user = await User.findById(req.params.id).select("-password -isAdmin");
+  res.send(user);
+});
 
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
-    
-    let user = await User.findOne({ email: req.body.email });
-    if (user) return res.status(400).send('User already registered.')
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    user = new User(_.pick(req.body, ['name', 'email', 'password']))
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-    await user.save();
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
 
-    res.send(user)
-})
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  res.send(user);
+});
+
+router.put("/:id", auth, async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  const oldPassword = await bcrypt.compare(req.body.oldPassword, user.password);
+  if (!oldPassword) return res.status(400).send("Invalid password.");
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(req.body.newPassword, salt);
+
+  schema = {
+    oldPassword: Joi.string().min(5).required(),
+    newPassword: Joi.string().min(5).required(),
+  };
+  
+  const {error} =  Joi.validate(req.body, schema)
+  if(error) return res.status(400).send(error.details[0].message)
+  
+  await user.save();
+  res.send(user);
+});
 
 module.exports = router;
