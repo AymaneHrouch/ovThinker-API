04079542dcf281ba3da1c92660050b4b07319684
Aymane Hrouch <jo.aymane@gmail.com>
2021-01-22 11:18:54 +0000

    add min(3) to password

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ function validateUser(user) {
   schema = {
     name: Joi.string().min(2).max(30).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string(),
+    password: Joi.string().min(5),
     isAdmin: Joi.boolean(),
   };
   return Joi.validate(user, schema);
