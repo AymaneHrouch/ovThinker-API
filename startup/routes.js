44ae0ca4958d1d37410c38997061c24e17519d64
Aymane Hrouch <jo.aymane@gmail.com>
2021-02-11 22:44:47 +0000

    fix Error: request entity too large request entity too large

--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const helmet = require("helmet");
 const users = require("../routes/users");
 const journals = require("../routes/journals");
 const auth = require("../routes/auth");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
-  app.use(helmet());
-  app.use(express.json());
+  app.use(express.json({ limit: "50mb" }));
   app.use("/api/users", users);
   app.use("/api/auth", auth);
   app.use("/api/journals", journals);
