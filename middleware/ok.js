57551cbc824b59c5da1086671cfc7f77beb0b691
Aymane Hrouch <jo.aymane@Gmail.com>
2022-12-09 13:12:01 +0100

    Update ok.js

--- middleware/ok.js
+++ middleware/ok.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
     app.use(function (req, res) {
-        res.status(404).send("Ooops motherfucker, this page doesn't exist hh.");
+        res.status(404).send("Ooops, this page doesn't exist hh.");
       });
 }
